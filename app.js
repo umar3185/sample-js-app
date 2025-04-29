e0b4aab881f74cdb342a5f400c16132ebf6a0394
Mohammed Umar <umar@brio.co.in>
2025-04-29 17:41:09 +0530

    updated8

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Brioites\n');
+  res.end('Hello all Brioites\n');
 });
 
 server.listen(port, hostname, () => {
